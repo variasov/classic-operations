b0130a0dbc2896184e86700111f6951273162869
Sergey Variasov <variasov@gmail.com>
2024-01-25 14:17:35 +0700

    Вынес doublewrap в компоненты, зависимость от компонентов теперь обязательна

--- sources/classic/operations/decorator.py
+++ sources/classic/operations/decorator.py
@@ -1,30 +1,8 @@
 from functools import wraps
 
-try:
-    from classic.components import add_extra_annotation
-except ImportError:
-    def add_extra_annotation(fn):
-        return fn
+from classic.components import add_extra_annotation, doublewrap
 
 from .operation import Operation
-
-
-def doublewrap(f):
-    """
-    Классный сниппет, облегчающий создание декораторов с параметрами.
-    Взято отсюда: https://stackoverflow.com/a/14412901
-    """
-
-    @wraps(f)
-    def new_dec(*args, **kwargs):
-        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
-            # actual decorated function
-            return f(args[0])
-        else:
-            # decorator arguments
-            return lambda realf: f(realf, *args, **kwargs)
-
-    return new_dec
 
 
 @doublewrap
